5170a9bd17afabc5f97fd9360f2de4ff61a98c40
suzuki-ai <karikakko10@gmail.com>
2019-09-19 15:11:26 +0900

    辞書型データのkeyとvariuを記述

---
+++ fruit.py
@@ -0,0 +1,16 @@
+def main():
+    fruits_dict = {
+        'りんご': 'apple',
+        'ぶどう': 'Grape',
+        'みかん': 'Orange',
+    }
+    #
+    print(fruits_dict['ぶどう'])
+    #
+    print(fruits_dict['みかん'])
+    #
+    print(fruits_dict['りんご'])
+
+
+if __name__ == '__main__':
+    main()
